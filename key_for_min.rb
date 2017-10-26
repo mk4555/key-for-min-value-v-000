c60532dd64175f03b20581ff21da41fd5a1f4bd1
Min Kim <mk4555@nyu.edu>
2017-10-26 14:59:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require('pry')
 def key_for_min_value(name_hash)
   min = nil
   ans = ""
@@ -12,6 +12,7 @@ def key_for_min_value(name_hash)
       min = value
       ans = key
     end
+    
   end
   ans
 end
